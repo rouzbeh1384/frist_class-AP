f4aa81c34106a1a8707c7f7c41cc1c3ccd339495
rouzbeh <r.84.asgari@gmail.com>
2024-02-21 09:41:44 +0330

    commit the test 1.cpp and 2.cpp

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "test.h"
+using namespace std;
+
+int main()
+{
+	int a;
+	int b ;
+	cin>>a;
+	cin>>b;
+	cout<<"sue :"<<sue(a,b)<<"/n";
+	cout<<pr(a,b);
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+
+int sue(int a,int b)
+{
+	return a*a+b;
+}
+int pr(int a,int b)
+{
+	return a/b+b;
+}
